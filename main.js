c3bebb649a5f435bd69da07504922a0cf229f9c7
JasonTodd69 <66045469+JasonTodd69@users.noreply.github.com>
2022-11-27 20:17:18 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+})
+
+
+ScrollReveal().reveal('.showcase', { delay:300});
+ScrollReveal().reveal('.offers', { delay:300});
+ScrollReveal().reveal('.banner-one', { delay:300});
+ScrollReveal().reveal('.banner-two', { delay:300});
+ScrollReveal().reveal('.social', { delay:300});
+
